984ecb5245373f754e9d9f15e9ad409f712deb0c
Javier_Luna <javy_luna_jr@hotmail.com>
2017-02-17 20:54:08 -0700

    1ER_EVA_7_MORE_ACTIONS

--- 1ER_EVA_7_MORE_ACTIONS/ViewController.swift
+++ 1ER_EVA_7_MORE_ACTIONS/ViewController.swift
@@ -9,7 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var txtFldNom: UITextField!
+    @IBOutlet weak var txtFldEdad: UITextField!
+    @IBOutlet weak var txtFldAll: UILabel!
+    
+    @IBAction func btnOk(sender: AnyObject) {
+        var sMensa = txtFldAll.text
+        txtFldAll.text = sMensa
+        
+        let acMiDialogo = UIAlertController(title: "Que onda que pex", message: sMensa, preferredStyle: .Alert)
+        let btnOk = UIAlertAction(title: "Fierro", style: .Cancel, handler: nil)
+        acMiDialogo.addAction(btnOk)
+        
+        presentViewController(acMiDialogo, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func onTapGestureRecognizer(sender: AnyObject) {
+        txtFldNom.resignFirstResponder()
+        txtFldEdad.resignFirstResponder()
+    }
+        
+    @IBAction func myTouch(sender: UIButton) {
 
+    }
+    
+    @IBAction func textFieldDoneEditing(sender: UITextField){
+    sender.resignFirstResponder()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
